4edeb8509306b5b108f1595598778b5e5811317c
ahmedrowaihi <ahmedrowaihi1@gmail.com>
2020-07-01 00:09:13 +0800

    fix functions

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
+const setTotalStatus= (data)=>{
+    let TotalDic = {
+        '0': data.cases,
+        '1': data.active,
+        '2': data.recovered,
+        '3': data.deaths,
+    }
+    let TotElments = document.querySelectorAll(".card-subtitle");
+    for(let totnum in TotElments){
+            TotElments[totnum].innerText = numeral(TotalDic[totnum]).format(0,0)
+        }
+};
 
 
-window.onload = () => {
-    getTotalcases();
-    getCountryData();
-    getHistoricalData();
-}
+const getCountryData = () => {
+    fetch("https://corona.lmao.ninja/v2/countries")
+    .then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        coronaGlobalData = data;
+        showDataOnMap(data);
+        showDataInTable(data);
+    })
+};
+
 
 
 var map;
 var infoWindow;
+let coronaGlobalData;
+let mapCircles = [];
+var casesTypeColors = {
+    cases: '#1d2c4d',
+    active: '#9d80fe',
+    recovered: '#7dd71d',
+    deaths: '#fb4443'
+}
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 39.8283, lng: -98.5795},
@@ -17,211 +43,24 @@ function initMap() {
     });
     infoWindow = new google.maps.InfoWindow();
 }
-
-const getTotalcases= () =>{
-    fetch("https://disease.sh/v2/all?yesterday=true")
-    .then((data) =>{
-            return data.json()
-    }).then((data)=>{
-        let setted = setTotalData(data);
-        buildpieChart(setted);
-    })
+var allcards = document.getElementsByClassName('card')
+const changeDataSelection = (casesType, that) => {
+    clearTheMap();
+    showDataOnMap(coronaGlobalData, casesType);
+    for(let i = 0; i < allcards.length; i++){
+        allcards[i].classList.remove('active-card')
+    }
+    that.classList.add('active-card')
 }
-const setTotalData= (data)=>{
-    let TotalDic = {
-        '0': numeral(data.cases).format('0,0'),
-        '1': numeral(data.active).format('0,0'),
-        '2': numeral(data.recovered).format('0,0'),
-        '3': numeral(data.deaths).format('0,0'),
+const clearTheMap = () => {
+    for(let circle of mapCircles){
+        circle.setMap(null);
     }
-    let TotElments = document.querySelectorAll(".card-subtitle");
-    for(let totnum in TotElments){
-            TotElments[totnum].innerText = TotalDic[totnum]
-        }
-        return TotalDic;
-};
-
-const getCountryData = () => {
-    fetch("https://corona.lmao.ninja/v2/countries")
-    .then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        showDataOnMap(data);
-        // showDataInTable(data);
-    })
-};
-
-const getHistoricalData = () => {
-    fetch("https://corona.lmao.ninja/v2/historical/all?lastdays=120")
-    .then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        let chartData = buildChartData(data);
-        buildlineChart(chartData);
-    })
-};
-
-const buildChartData = (data) => {
-    let chartData = {
-        cases: [],
-        recovered: [],
-        deaths: [],
-    };
-    for(kind in data){
-        for(actualdata in data[kind]){
-            let newdatapoint = {
-                x: actualdata,
-                y: data[kind][actualdata]
-            };
-            chartData[kind].push(newdatapoint);
-        };
-    };
-    return chartData;
-};
+}
 
 
-const buildlineChart = (chartData) => {
-    var timeFormat = 'MM/DD/YY';
-    var ctx = document.getElementById('Total-Chart').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-        // The data for our dataset
-        data: {
-            datasets: [
-                {
-                    label: 'Total Deaths',
-                    data: chartData.deaths,
-                    backgroundColor: '#FF4136',
-                    // borderColor: '#FF4136',
-                    // pointStyle: 'circle',
-                    // fill: '-1',
-                    borderWidth: 0,
-                    radius: 0.3,
-                },
-                {
-                    label: 'Total Recovered',
-                    data: chartData.recovered,
-                    backgroundColor: '#01FF70',
-                    // borderColor: '#01FF70',
-                    // pointStyle: 'circle',
-                    // fill: 'true',
-                    borderWidth: 0,
-                    radius: 0.3,
-                },
-                {
-                label: 'Total Cases',
-                data: chartData.cases,
-                backgroundColor: '#0074D9',
-                // borderColor: '#0074D9',
-                // pointStyle: 'circle',
-                // fill: true,
-                borderWidth: 0,
-                radius: 0.3,
-            },
-
-        ]
-        },
-
-        // Configuration options go here
-        options: {
-            title: {
-                display: true,
-                text: 'Line Chart'
-              },
-            maintainAspectRatio: false,
-            tooltips: {
-                mode: 'index',
-                intersect: false
-            },
-            scales:     {
-                xAxes: [{
-                    type: "time",
-                    time: {
-                        format: timeFormat,
-                        tooltipFormat: 'll'
-                    }
-                }],
-                yAxes: [{
-                    ticks: {
-                        callback: function(value, index, values) {
-                            return numeral(value).format('0,0');
-                        }
-                    }
-                }],
-                animation: {
-                    animateScale: true,
-                    animateRotate: true
-                },
-                responsive: true,
-                    legend: {
-                position: 'top',
-                },
-            }
-        }
-    });
-};
-
-const buildpieChart = (setted) =>{
-    var ctx = document.getElementById('pie')
-    var myPieChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: 
-        {
-            labels: ['Deaths', 'Active', 'Recovered'],
-            datasets: [
-             /* Outer doughnut data starts*/
-            {
-              data: [
-                numeral(setted['3']).value(),
-                numeral(setted['2']).value(),
-                numeral(setted['1']).value()
-              ],
-              backgroundColor: [
-                "#FF4136", // red
-                "#01FF70", // green
-                "rgb(54, 162, 235)", //blue
-              ],
-            },
-            ]
-
-        },
-        options: {
-            responsive: true,
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'Doughnut Chart'
-            },
-            animation: {
-              animateScale: true,
-              animateRotate: true
-            },
-            tooltips: {
-              callbacks: {
-                label: function(item, data) {
-                    return data.datasets[item.datasetIndex].label+ ": "+ data.labels[item.index]+ ": "+ data.datasets[item.datasetIndex].data[item.index];
-                }
-            }
-        }
-    }
-        
-
-    });
-
-};
-
-
-
-const openInfoWindow = () => {
-    infoWindow.open(map);
-};
-
-
-const showDataOnMap = (data) => {
+const showDataOnMap = (data, casesType="cases") => {
+    
     data.map((country)=>{
         let countryCenter = {
             lat: country.countryInfo.lat,
@@ -229,15 +68,17 @@ const showDataOnMap = (data) => {
         }
 
         var countryCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
+            strokeColor: casesTypeColors[casesType],
             strokeOpacity: 0.8,
             strokeWeight: 2,
-            fillColor: '#FF0000',
+            fillColor: casesTypeColors[casesType],
             fillOpacity: 0.35,
             map: map,
             center: countryCenter,
-            radius: country.cases
+            radius: country[casesType]
         });
+
+        mapCircles.push(countryCircle);
 
         var html = `
             <div class="info-container">
@@ -274,17 +115,46 @@ const showDataOnMap = (data) => {
 
 };
 
-// const showDataInTable = (data) => {
-//     var html = '';
-//     data.forEach((country)=>{
-//         html += `
-//         <tr>
-//             <td>${country.country}</td>
-//             <td>${country.cases}</td>
-//             <td>${country.recovered}</td>
-//             <td>${country.deaths}</td>
-//         </tr>
-//         `
-//     })
-//     document.getElementById('table-data').innerHTML = html;
-// };
+
+
+
+const showDataInTable = (data) => {
+    var html = '';
+    data.forEach((country)=>{
+        html += `
+        <tr>
+            <td>${country.country}</td>
+            <td>${country.cases}</td>
+            <td>${country.recovered}</td>
+            <td>${country.deaths}</td>
+        </tr>
+        `
+    })
+    document.getElementById('table-data').innerHTML = html;
+};
+
+const getWorldCoronaData= () =>{
+    fetch("https://disease.sh/v2/all")
+    .then((data) =>{
+            return data.json()
+    }).then((data)=>{
+        buildpieChart(data);
+        setTotalStatus(data);
+    })
+}
+
+const getHistoricalData = () => {
+    fetch("https://corona.lmao.ninja/v2/historical/all?lastdays=120")
+    .then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        let chartData = buildChartlineData(data);
+        buildlineChart(chartData);
+    })
+};
+
+window.onload = () => {
+    getCountryData();
+    getHistoricalData();
+    getWorldCoronaData();
+}
