46261708108a0b4df0ec5e59bbdacb7b79dcab87
ahmedrowaihi <67356781+ahmedrowaihi@users.noreply.github.com>
2020-07-21 01:39:26 +0800

    Update charts.js

--- charts.js
+++ charts.js
@@ -30,7 +30,6 @@ const buildChartlineData = (data) => {
 //   getHistoricalData();
 // };
 const buildlineChart = (chartData) => {
-  console.log(chartData);
   var timeFormat = "MM/DD/YY";
   var ctx = document.getElementById("chart-area").getContext("2d");
   var chart = new Chart(ctx, {
